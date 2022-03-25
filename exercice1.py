71e92bcfbb294a15eb12e11270bde48201006c01
Mélanie Thomas <melanie.thomas.design@gmail.com>
2022-03-25 11:35:54 +0100

    test

---
+++ exercice1.py
@@ -0,0 +1,9 @@
+def characters_count():
+    phrase = input("please enter your phrase: ")
+    characters_nb = str(len(phrase))
+    message = "your phrase has " + characters_nb + " characters"
+    print(message)
+
+characters_count()
+characters_count()
+characters_count()
